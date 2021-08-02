934d8527d7d3a835fe745ccb5a34f6b292096e16
irudayan <irudayan111@gmail.com>
2021-08-02 14:45:50 +0530

    mini

---
+++ footer.php
@@ -0,0 +1,15 @@
+<<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>date function</title>
+</head>
+<body>
+
+
+<?php
+echo "".date("y/d/m")."<br>";
+
+?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>header</title>
+</head>
+<body>
+<?php
+switch ("red") {
+	case 'red':
+		your favarite color red;
+		break;
+	
+	default:
+		color is pink;
+		break;
+}
+
+?>
+</body>
+</html>
